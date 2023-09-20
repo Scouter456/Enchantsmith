d2fa8769e46b0102befeb4c2a3bfeb066d200405
Scouter456 <markveenstra14@gmail.com>
2023-09-20 18:23:30 +0200

    Fixed a bug where opening the menu would kick player in multiplayer

--- src/main/java/com/scouter/enchantsmith/menu/ESMenus.java
+++ src/main/java/com/scouter/enchantsmith/menu/ESMenus.java
@@ -2,17 +2,21 @@ package com.scouter.enchantsmith.menu;
 
 import com.mojang.logging.LogUtils;
 import com.scouter.enchantsmith.EnchantSmith;
-import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.core.Registry;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
 import org.slf4j.Logger;
 
+import static com.scouter.enchantsmith.EnchantSmith.prefix;
+
 public class ESMenus {
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public static final MenuType<EnchantSmithMenu> ENCHANTSMITH_MENU = new MenuType<>(EnchantSmithMenu::new);
+    public static final MenuType<EnchantSmithMenu> ENCHANTSMITH_MENU = register("enchantsmith_menu", EnchantSmithMenu::new);
 
+    private static <T extends AbstractContainerMenu> MenuType register(String key, MenuType.MenuSupplier<T> factory) {
+        return Registry.register(Registry.MENU, prefix(key), new MenuType(factory));
+    }
 
 
     public static void MENUS()
