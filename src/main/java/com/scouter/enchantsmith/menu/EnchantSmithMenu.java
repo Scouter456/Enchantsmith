c503ea39e921f7a98392edcc33a03077a084d798
Scouter456 <markveenstra14@gmail.com>
2023-09-21 07:47:26 +0200

    Fix dupe bug when exiting menu

--- src/main/java/com/scouter/enchantsmith/menu/EnchantSmithMenu.java
+++ src/main/java/com/scouter/enchantsmith/menu/EnchantSmithMenu.java
@@ -572,7 +572,7 @@ public class EnchantSmithMenu extends AbstractContainerMenu {
         super.removed(pPlayer);
         playCloseSound();
         this.trader.setTradingPlayer((Player)null);
-        Fix dupe bug when exiting menu        if (!this.trader.isClientSide()) {
+        if (!this.trader.isClientSide()) {
             if (!pPlayer.isAlive() || pPlayer instanceof ServerPlayer && ((ServerPlayer)pPlayer).hasDisconnected()) {
                 this.removeItemsFromContainer(this.container, pPlayer, 0);
                 this.removeItemsFromContainer(this.emeraldContainer, pPlayer, 0);
